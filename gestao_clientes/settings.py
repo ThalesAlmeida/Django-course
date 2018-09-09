0359bc5db0a804a6b4aee6c8851ff3cc99dc4b3f
Thales <thalesalves44@gmail.com>
2018-09-09 19:58:39 -0300

    Adicionando middleware

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -51,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'middlewares.MetaData.MetaData',
 ]
 
 ROOT_URLCONF = 'gestao_clientes.urls'
